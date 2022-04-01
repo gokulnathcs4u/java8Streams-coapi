b81888f9347f4173b50cf0d94965ac4039e488d9
gokulnathcs4u <gokulnathcs4u@gmail.com>
2022-04-01 08:29:38 +0530

    initial commit

---
+++ src/main/java/com/java8streams/coapi/Java8streamsCoapiApplication.java
@@ -0,0 +1,13 @@
+package com.java8streams.coapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8streamsCoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8streamsCoapiApplication.class, args);
+	}
+
+}
